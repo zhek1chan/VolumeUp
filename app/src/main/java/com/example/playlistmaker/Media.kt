10df95cffdcaebdccf8127b89c384675baf9f738
zhek1chan <evg.ermolov@yandex.ru>
2023-04-17 12:26:30 +0500

    the birth of app

---
+++ app/src/main/java/com/example/playlistmaker/Media.kt
@@ -0,0 +1,10 @@
+package com.example.playlistmaker
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Media: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mediateka)
+    }
+}
