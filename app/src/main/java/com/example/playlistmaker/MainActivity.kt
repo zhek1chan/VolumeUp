7013c0f25e7a379317a73e6ce7f5147dce6ddcfa
zhek1chan <evg.ermolov@yandex.ru>
2023-04-18 12:59:02 +0500

    2.0

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.widget.Button
 //import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-//
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
